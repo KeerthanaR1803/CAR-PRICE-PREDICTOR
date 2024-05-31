d93daab49ff573330d70c701c3d437558d67c98b
KeerthanaR1803 <167592786+KeerthanaR1803@users.noreply.github.com>
2024-05-31 12:00:35 +0530

    Add files via upload

---
+++ CAR_PRICE_PREDICTOR.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import datetime 
+import xgboost as xgb 
+import streamlit as st
+
+
+def main():
+    html_temp="""
+    <div style = "background-color:lightblue;padding:16px">
+    <h2 style="color:black;text-align:center;"> Car Proce Prediction using ML
+    </div>
+    """ 
+    
+    st.markdown(html_temp.unsafe_allow_html==True)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    if ___name___ == '___main___':
+        main()
